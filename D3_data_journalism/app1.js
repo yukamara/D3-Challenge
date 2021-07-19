006fa9eafd9d7dac83e86dda94f2a9a08c518ec1
Yusufu U Kamara <59753837+yukamara@users.noreply.github.com>
2021-07-19 10:52:21 -0700

    Created svg element and dimentions

---
+++ D3_data_journalism/app1.js
@@ -0,0 +1,29 @@
+// Define SVG area dimensions
+var svgWidth =900;
+var svgHeight = 700;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+//***********************************************
